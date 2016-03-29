614e6bc2032080c51e131aab5605cd852464cff4
raphaelbp12 <raphaelbp12@gmail.com>
2016-03-29 20:58:10 -0300

    Initial Files

---
+++ mp/src/game/shared/hl2mp/weapon_ak47.cpp
@@ -0,0 +1,389 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "npcevent.h"
+#include "in_buttons.h"
+
+#ifdef CLIENT_DLL
+	#include "c_hl2mp_player.h"
+#else
+	#include "hl2mp_player.h"
+#endif
+
+#include "weapon_hl2mpbasehlmpcombatweapon.h"
+
+#define	PISTOL_FASTEST_REFIRE_TIME		0.1f
+#define	PISTOL_FASTEST_DRY_REFIRE_TIME	0.2f
+
+#define	PISTOL_ACCURACY_SHOT_PENALTY_TIME		0.2f	// Applied amount of time each shot adds to the time we must recover from
+#define	PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME	1.5f	// Maximum penalty to deal out
+
+#ifdef CLIENT_DLL
+#define CWeaponAK47 C_WeaponAK47
+#endif
+
+//-----------------------------------------------------------------------------
+// CWeaponAK47
+//-----------------------------------------------------------------------------
+
+class CWeaponAK47 : public CBaseHL2MPCombatWeapon
+{
+public:
+	DECLARE_CLASS( CWeaponAK47, CBaseHL2MPCombatWeapon );
+
+	CWeaponAK47(void);
+
+	DECLARE_NETWORKCLASS(); 
+	DECLARE_PREDICTABLE();
+
+	void	Precache( void );
+	void	ItemPostFrame( void );
+	void	ItemPreFrame( void );
+	void	ItemBusyFrame( void );
+	void	PrimaryAttack( void );
+	void	AddViewKick( void );
+	void	DryFire( void );
+	void	DrawHitmarker(void);
+
+	void	UpdatePenaltyTime( void );
+
+	Activity	GetPrimaryAttackActivity( void );
+
+	virtual bool Reload( void );
+
+	virtual const Vector& GetBulletSpread( void )
+	{		
+		static Vector cone;
+
+		float ramp = RemapValClamped(	m_flAccuracyPenalty, 
+											0.0f, 
+											PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME, 
+											0.0f, 
+											1.0f ); 
+
+			// We lerp from very accurate to inaccurate over time
+		VectorLerp( VECTOR_CONE_1DEGREES, VECTOR_CONE_6DEGREES, ramp, cone );
+
+		return cone;
+	}
+	
+	virtual int	GetMinBurst() 
+	{ 
+		return 1; 
+	}
+
+	virtual int	GetMaxBurst() 
+	{ 
+		return 3; 
+	}
+
+	virtual float GetFireRate( void ) 
+	{
+		return 0.080f; 
+	}
+	
+#ifndef CLIENT_DLL
+	DECLARE_ACTTABLE();
+#endif
+
+private:
+	CNetworkVar( float,	m_flSoonestPrimaryAttack );
+	CNetworkVar( float,	m_flLastAttackTime );
+	CNetworkVar( float,	m_flAccuracyPenalty );
+	CNetworkVar( int,	m_nNumShotsFired );
+
+private:
+	CWeaponAK47( const CWeaponAK47 & );
+};
+
+IMPLEMENT_NETWORKCLASS_ALIASED( WeaponAK47, DT_WeaponAK47 )
+
+BEGIN_NETWORK_TABLE( CWeaponAK47, DT_WeaponAK47 )
+#ifdef CLIENT_DLL
+	RecvPropTime( RECVINFO( m_flSoonestPrimaryAttack ) ),
+	RecvPropTime( RECVINFO( m_flLastAttackTime ) ),
+	RecvPropFloat( RECVINFO( m_flAccuracyPenalty ) ),
+	RecvPropInt( RECVINFO( m_nNumShotsFired ) ),
+#else
+	SendPropTime( SENDINFO( m_flSoonestPrimaryAttack ) ),
+	SendPropTime( SENDINFO( m_flLastAttackTime ) ),
+	SendPropFloat( SENDINFO( m_flAccuracyPenalty ) ),
+	SendPropInt( SENDINFO( m_nNumShotsFired ) ),
+#endif
+END_NETWORK_TABLE()
+
+#ifdef CLIENT_DLL
+BEGIN_PREDICTION_DATA( CWeaponAK47 )
+	DEFINE_PRED_FIELD( m_flSoonestPrimaryAttack, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
+	DEFINE_PRED_FIELD( m_flLastAttackTime, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
+	DEFINE_PRED_FIELD( m_flAccuracyPenalty, FIELD_FLOAT, FTYPEDESC_INSENDTABLE ),
+	DEFINE_PRED_FIELD( m_nNumShotsFired, FIELD_INTEGER, FTYPEDESC_INSENDTABLE ),
+END_PREDICTION_DATA()
+#endif
+
+LINK_ENTITY_TO_CLASS( weapon_ak47, CWeaponAK47 );
+PRECACHE_WEAPON_REGISTER( weapon_ak47 );
+
+#ifndef CLIENT_DLL
+acttable_t CWeaponAK47::m_acttable[] = 
+{
+	{ ACT_HL2MP_IDLE,					ACT_HL2MP_IDLE_PISTOL,					false },
+	{ ACT_HL2MP_RUN,					ACT_HL2MP_RUN_PISTOL,					false },
+	{ ACT_HL2MP_IDLE_CROUCH,			ACT_HL2MP_IDLE_CROUCH_PISTOL,			false },
+	{ ACT_HL2MP_WALK_CROUCH,			ACT_HL2MP_WALK_CROUCH_PISTOL,			false },
+	{ ACT_HL2MP_GESTURE_RANGE_ATTACK,	ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL,	false },
+	{ ACT_HL2MP_GESTURE_RELOAD,			ACT_HL2MP_GESTURE_RELOAD_PISTOL,		false },
+	{ ACT_HL2MP_JUMP,					ACT_HL2MP_JUMP_PISTOL,					false },
+	{ ACT_RANGE_ATTACK1,				ACT_RANGE_ATTACK_PISTOL,				false },
+};
+
+
+IMPLEMENT_ACTTABLE( CWeaponAK47 );
+
+#endif
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CWeaponAK47::CWeaponAK47( void )
+{
+	m_flSoonestPrimaryAttack = gpGlobals->curtime;
+	m_flAccuracyPenalty = 0.0f;
+
+	m_fMinRange1		= 24;
+	m_fMaxRange1		= 1500;
+	m_fMinRange2		= 24;
+	m_fMaxRange2		= 200;
+
+	m_bFiresUnderwater	= true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponAK47::Precache( void )
+{
+	BaseClass::Precache();
+}
+
+
+void CWeaponAK47::DrawHitmarker(void)
+{
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+	{
+		return;
+	}
+
+#ifndef CLIENT_DLL
+	CSingleUserRecipientFilter filter(pPlayer);
+	UserMessageBegin(filter, "ShowHitmarker");
+	WRITE_BYTE(1);
+	MessageEnd();
+#endif
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CWeaponAK47::DryFire( void )
+{
+	WeaponSound( EMPTY );
+	SendWeaponAnim( ACT_VM_DRYFIRE );
+	
+	m_flSoonestPrimaryAttack	= gpGlobals->curtime + PISTOL_FASTEST_DRY_REFIRE_TIME;
+	m_flNextPrimaryAttack		= gpGlobals->curtime + SequenceDuration();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CWeaponAK47::PrimaryAttack( void )
+{
+	if ( ( gpGlobals->curtime - m_flLastAttackTime ) > 0.5f )
+	{
+		m_nNumShotsFired = 0;
+	}
+	else
+	{
+		m_nNumShotsFired++;
+	}
+
+	m_flLastAttackTime = gpGlobals->curtime;
+	m_flSoonestPrimaryAttack = gpGlobals->curtime + PISTOL_FASTEST_REFIRE_TIME;
+
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+	if (!pPlayer)
+	{
+		return;
+	}
+
+	if( pOwner )
+	{
+		// Each time the player fires the pistol, reset the view punch. This prevents
+		// the aim from 'drifting off' when the player fires very quickly. This may
+		// not be the ideal way to achieve this, but it's cheap and it works, which is
+		// great for a feature we're evaluating. (sjb)
+		pOwner->ViewPunchReset();
+	}
+
+	BaseClass::PrimaryAttack();
+
+	// Add an accuracy penalty which can move past our maximum penalty time if we're really spastic
+	m_flAccuracyPenalty += PISTOL_ACCURACY_SHOT_PENALTY_TIME;
+
+	// set up the vectors and traceline
+	trace_t tr;
+	Vector	vecStart, vecStop, vecDir;
+
+	// get the angles
+	AngleVectors(pPlayer->EyeAngles(), &vecDir);
+
+	// get the vectors
+	vecStart = pPlayer->Weapon_ShootPosition();
+	vecStop = vecStart + vecDir * MAX_TRACE_LENGTH;
+
+	// do the traceline
+	UTIL_TraceLine(vecStart, vecStop, MASK_ALL, pPlayer, COLLISION_GROUP_NONE, &tr);
+	
+	// check to see if we hit a Player
+	if (tr.m_pEnt)
+	{
+		if (tr.m_pEnt->GetTeamNumber() != pPlayer->GetTeamNumber())
+		{
+			if (tr.m_pEnt->IsPlayer())
+			{
+				DrawHitmarker();
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponAK47::UpdatePenaltyTime( void )
+{
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if ( pOwner == NULL )
+		return;
+
+	// Check our penalty time decay
+	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
+	{
+		m_flAccuracyPenalty -= gpGlobals->frametime;
+		m_flAccuracyPenalty = clamp( m_flAccuracyPenalty, 0.0f, PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponAK47::ItemPreFrame( void )
+{
+	UpdatePenaltyTime();
+
+	BaseClass::ItemPreFrame();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponAK47::ItemBusyFrame( void )
+{
+	UpdatePenaltyTime();
+
+	BaseClass::ItemBusyFrame();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Allows firing as fast as button is pressed
+//-----------------------------------------------------------------------------
+void CWeaponAK47::ItemPostFrame( void )
+{
+	BaseClass::ItemPostFrame();
+
+	if ( m_bInReload )
+		return;
+	
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if ( pOwner == NULL )
+		return;
+	
+	if ( pOwner->m_nButtons & IN_ATTACK2 )
+	{
+		m_flLastAttackTime = gpGlobals->curtime + PISTOL_FASTEST_REFIRE_TIME;
+		m_flSoonestPrimaryAttack = gpGlobals->curtime + PISTOL_FASTEST_REFIRE_TIME;
+		m_flNextPrimaryAttack = gpGlobals->curtime + PISTOL_FASTEST_REFIRE_TIME;
+	}
+
+	//Allow a refire as fast as the player can click
+	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
+	{
+		m_flNextPrimaryAttack = gpGlobals->curtime - 0.1f;
+	}
+	else if ( ( pOwner->m_nButtons & IN_ATTACK ) && ( m_flNextPrimaryAttack < gpGlobals->curtime ) && ( m_iClip1 <= 0 ) )
+	{
+		DryFire();
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : int
+//-----------------------------------------------------------------------------
+Activity CWeaponAK47::GetPrimaryAttackActivity( void )
+{
+	if ( m_nNumShotsFired < 1 )
+		return ACT_VM_PRIMARYATTACK;
+
+	if ( m_nNumShotsFired < 2 )
+		return ACT_VM_RECOIL1;
+
+	if ( m_nNumShotsFired < 3 )
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool CWeaponAK47::Reload( void )
+{
+	bool fRet = DefaultReload( GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD );
+	if ( fRet )
+	{
+		WeaponSound( RELOAD );
+		m_flAccuracyPenalty = 0.0f;
+	}
+	return fRet;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponAK47::AddViewKick( void )
+{
+	CBasePlayer *pPlayer  = ToBasePlayer( GetOwner() );
+	
+	if ( pPlayer == NULL )
+		return;
+
+	QAngle	viewPunch;
+
+	viewPunch.x = SharedRandomFloat( "pistolpax", 0.25f, 0.5f );
+	viewPunch.y = SharedRandomFloat( "pistolpay", -.6f, .6f );
+	viewPunch.z = 0.0f;
+
+	//Add it to the view punch
+	pPlayer->ViewPunch( viewPunch );
+}
